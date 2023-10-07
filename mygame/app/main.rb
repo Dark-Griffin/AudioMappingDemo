9669d5db87dde558199a3c52102306f112b18687
Dark-Griffin <gawain@moonbow-ent.com>
2023-10-07 16:19:54 -0400

    First public version of code
    - cleaned up code, potential github version maybe?  Need to make sure it works first.

--- mygame/app/main.rb
+++ mygame/app/main.rb
@@ -23,12 +23,11 @@ def tick args
   # we also define a playback delay, this will control how often the sound is played from that point in space.
   # each will need an angle value for use later in the system when we calculate the angle from the listener to the sound, and a distance value to control volume.
   # also, we need a max cutoff distance, this is used to give a range for "I can hear this sound", and if distance is greater than this, we don't play the sound.
-  args.state.audio_map[:sound1] = { path: 'sounds/splash.wav', x: 100, y: 100, delay: 2.0, angle: 0.0, distance: 0.0, max_distance: 100 }
-  #args.state.audio_map[:sound2] = { path: 'sounds/spring.wav', x: 350, y: 400, delay: 1.0, angle: 0.0, distance: 0.0, max_distance: 100 }
-  #args.state.audio_map[:sound3] = { path: 'sounds/tada.wav', x: 700, y: 100, delay: 4.0, angle: 0.0, distance: 0.0, max_distance: 100 }
-  #args.state.audio_map[:sound4] = { path: 'sounds/tink.wav', x: 650, y: 400, delay: 0.5, angle: 0.0, distance: 0.0, max_distance: 100 }
+  args.state.audio_map[:sound1] ||= { path: 'sounds/splash.wav', x: 100, y: 100, delay: 100.0, angle: 0.0, distance: 0.0, max_distance: 250, last_played_tick: 0 }
+  args.state.audio_map[:sound2] ||= { path: 'sounds/spring.wav', x: 350, y: 400, delay: 100.0, angle: 0.0, distance: 0.0, max_distance: 200, last_played_tick: 0 }
+  args.state.audio_map[:sound3] ||= { path: 'sounds/tada.wav', x: 700, y: 100, delay: 100.0, angle: 0.0, distance: 0.0, max_distance: 450, last_played_tick: 0 }
+  args.state.audio_map[:sound4] ||= { path: 'sounds/tink.wav', x: 650, y: 400, delay: 100.0, angle: 0.0, distance: 0.0, max_distance: 300, last_played_tick: 0 }
 
-  
   # for each audio in audio_map, set the listening angle from the player position
   args.state.audio_map.each do |key, value|
     # calculate the angle from the sound to the listener
@@ -37,33 +36,60 @@ def tick args
     value[:distance] = Math.sqrt((value[:x] - args.state.listener.x) ** 2 + (value[:y] - args.state.listener.y) ** 2)
   end
 
-  # todo for me
-  # - figure out listening pan from angle of sound relative to the listener
-  # - figure out listening volume from distance of sound relative to the listener
-  # - output sounds and play them based on the above calculations
-  # - keep track of ticks per sound so we can play the sounds at the specified delay times
-
   # for each sound in the audio_map, we need to get the angle between 0 and 180 degrees.  We also need to convert the backwards angles to be positive.  Then we need to store each angle relative to the listener angle so that the final resulting value is a measure of the angle between 0 and 1280 in audio pixel position space instead.
   args.state.audio_map.each do |key, value|
+    #calculate the angle difference between this sound's angle from viewer on 0 rotation, and the listener plane angle with their rotation considered.
     panning_angle = value[:angle] - args.state.listener.angle
-    #flip angle along the axis if it is over 180 degrees in either rotation direction
+    #flip the panning angle along the axis if it is over 180 degrees in either rotation direction
+    # this ensures that sounds behind the listener plane will still be panned correctly
     if panning_angle > Math::PI / 2
       panning_angle = -panning_angle + (Math::PI)
     end
     if panning_angle < -Math::PI / 2
       panning_angle = -panning_angle - (Math::PI)
     end
-    
-    #convert angle Math::PI /2 to 0 to 1280 range
-    panning_angle = (panning_angle + Math::PI / 2) / Math::PI * 1280
-    #store the angle in the audio_map
+    #convert our panning angle to an audio panning value between -1 and 1
+    panning_angle = panning_angle / (Math::PI / 2)
+    #store the panning_angle in our audio map so we can play the correct pan position later.
     value[:panning_angle] = panning_angle
   end
-  
-  # debug, show the panning_angle as a string on screen
-  args.outputs.labels << [640, 550, "panning_angle: #{args.state.audio_map[:sound1][:panning_angle]}", 5, 1]
-  # debug, show listener angle
-  args.outputs.labels << [640, 520, "listener angle: #{args.state.listener.angle}", 5, 1]
+
+  # for each sound, we also need to get the volume based on distance between listener and the sound.
+  args.state.audio_map.each do |key, value|
+    # first get the range of sound, if over the max distance, we skip this sound.
+    if value[:distance] > value[:max_distance]
+      # set volume to 0 for being too far away
+      value[:volume] = 0
+      #also stop any currently playing sounds that are now out of range
+      args.audio[key] = nil
+      next
+    end
+    # calculate the volume based on the distance from the listener to the sound, using the max distance as the range.
+    volume = 1 - value[:distance] / value[:max_distance]
+    # store the volume in our audio map so we can play the correct volume later.
+    value[:volume] = volume
+  end
+
+  # update all the sound ticks so we know if they should play this tick or not
+  args.state.audio_map.each do |key, value|
+    if value[:last_played_tick] + value[:delay] < args.tick_count
+      value[:last_played_tick] = args.tick_count
+    end
+  end
+
+  # now we can play the sounds as needed to recreate what the listener hears from this point in our audio map.
+  args.state.audio_map.each do |key, value|
+    #if sound is too far away, skip it
+    if value[:distance] > value[:max_distance]
+      next
+    end
+    #if the tick_count equals this last_played_tick, then we play the sound
+    if value[:last_played_tick] + value[:delay] == args.tick_count
+      #play the sound with the volume and panning_angle we calculated earlier
+      args.audio[key] = { input: value[:path], gain: value[:volume], x: value[:panning_angle] }
+      puts "playing sound #{key} at volume #{value[:volume]} and panning #{value[:panning_angle]}"
+    end
+  end
 
   # #############################################
   # interactions
